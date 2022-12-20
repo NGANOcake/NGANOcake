b95d6c9246bf022ef64e9382571f45657103db69
YojiGoto <117175808+yoji-goto@users.noreply.github.com>
2022-12-20 04:25:10 +0000

    [Add]商品詳細画面の追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ApplicationRecord
   has_many :order_details
 
   has_one_attached :image
+
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
 end
